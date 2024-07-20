bd9b6f05ec08797c2f70565109437d6237bd49b1
Velten Stoyann <krysto.dev@gmail.com>
2024-07-21 03:52:47 +1100

    details recyclable product and update recyclable product screens

--- frontend/src/screens/admin/AdminRecyclableProductDetailsScreen.jsx
+++ frontend/src/screens/admin/AdminRecyclableProductDetailsScreen.jsx
@@ -2,18 +2,73 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import Button from '../../components/shared/Button'
 import { ArrowBigLeft } from 'lucide-react'
+import { useGetRecyclableProductDetailsQuery } from '../../slices/recyclableProductApiSlice'
+import Barcode from 'react-barcode'
+import Rating from '../../components/shared/Rating'
 
 const AdminRecyclableProductDetailsScreen = () => {
-    const {id: recyclableProductId} = useParams()
+  const { id: recyclableProductId } = useParams()
+  const { data: recyclableProduct, error: recyclableProductError, isLoading: recyclableProductLoading } = useGetRecyclableProductDetailsQuery(recyclableProductId)
 
+  if (recyclableProductLoading) {
+    return <p>Chargement...</p>
+  }
+
+  if (recyclableProductError) {
+    return <p>Erreur de chargement des détails du produit recyclable.</p>
+  }
+
+  if (!recyclableProduct) {
+    return <p>Aucun produit trouvé.</p>
+  }
 
   return (
-    <>
-    <div className='flex items-center justify-between'>
-    <h1 className='text-2xl text-gray-800' >Produit recyclable {recyclableProductId}</h1>
-    <Button url={'/admin-produits-recyclable'}> <ArrowBigLeft/>Retour</Button>
+    <div className="p-4 max-w-7xl mx-auto">
+      <div className="flex items-center justify-between mb-6">
+        <h1 className='text-2xl font-semibold text-gray-800'>Détails du produit recyclable</h1>
+        <Button url={'/admin-produits-recyclable'}><ArrowBigLeft /> Retour</Button>
+      </div>
+
+      <div className="bg-white shadow-lg rounded-lg overflow-hidden relative">
+        {/* Code-barres positionné en haut à droite du composant */}
+        <div className="absolute top-4 right-4">
+          <Barcode
+            value={recyclableProduct.barCode}
+            format="EAN13"
+            displayValue={true}
+            width={2}
+            height={60}
+            background="#ffffff"
+            lineColor="#000000"
+          />
+        </div>
+        
+        <div className="flex">
+          {/* Section de l'image */}
+          <div className="w-1/3 p-4">
+            <img
+              src={recyclableProduct.images[0] || '/uploads/no-photo.png'}
+              alt={recyclableProduct.name}
+              className="w-full h-auto object-cover"
+            />
+          </div>
+
+          {/* Détails du produit */}
+          <div className="w-2/3 p-6">
+            <h2 className="text-3xl font-bold mb-4">{recyclableProduct.name}</h2>
+            <p className="text-gray-700 mb-2"><strong>Marque:</strong> {recyclableProduct.brand}</p>
+            <p className="text-gray-700 mb-2"><strong>Catégorie:</strong> {recyclableProduct.category}</p>
+            <p className="text-gray-700 mb-2"><strong>Poids:</strong> {recyclableProduct.weightGr} g</p>
+            <p className="text-gray-700 mb-2"><strong>Description:</strong> {recyclableProduct.description}</p>
+            <div className="flex items-center justify-between bg-gray-600 p-2 rounded-md">
+
+            <p className="text-gray-200">Note de Recyclage:</p>
+            <Rating value={recyclableProduct.recyclingNote} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    </>
   )
 }
 
--- frontend/src/screens/admin/AdminRecyclableProductEditScreen.jsx
+++ frontend/src/screens/admin/AdminRecyclableProductEditScreen.jsx
@@ -1,16 +1,222 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import {
+  useGetRecyclableProductDetailsQuery,
+  useUpdateRecyclableProductMutation,
+  useUploadRecyclableProductImageMutation,
+} from '../../slices/recyclableProductApiSlice'
+import Loader from '../FeedbackScreens/Loader'
 import Button from '../../components/shared/Button'
-import { ArrowBigLeft } from 'lucide-react'
-import { useParams } from 'react-router-dom'
+import { ArrowBigLeft, Loader2, Send } from 'lucide-react'
+import { toast } from 'react-toastify'
 
 const AdminRecyclableProductEditScreen = () => {
-    const {id: recyclableProductId} = useParams()
+  const { id: recyclableProductId } = useParams()
+  const navigate = useNavigate()
+
+  // States for form fields
+  const [name, setName] = useState('')
+  const [brand, setBrand] = useState('')
+  const [category, setCategory] = useState('')
+  const [weightGr, setWeightGr] = useState('')
+  const [description, setDescription] = useState('')
+  const [recyclingNote, setRecyclingNote] = useState('')
+  const [imagesArray, setImagesArray] = useState([])
+  const [barCode, setBarCode] = useState('')
+
+  // List of categories
+  const categories = ['Alimentaires', 'Beauté', 'Entretien', 'Autres']
+
+  // List of recycling notes
+  const recyclingNotes = [ 1, 1.5, 2, 2.5, 3,3.5,4,4.5] // Replace with actual notes
+
+  // Query and mutations
+  const { data: recyclableProduct, error: recyclableProductError, isLoading: recyclableProductLoading, refetch } = useGetRecyclableProductDetailsQuery(recyclableProductId)
+  const [updateRecyclableProduct, { isLoading: loadingUpdate }] = useUpdateRecyclableProductMutation()
+  const [uploadRecyclableProductImage, { isLoading: uploadMutationLoading }] = useUploadRecyclableProductImageMutation()
+
+  // Effect to set form values from fetched data
+  useEffect(() => {
+    if (recyclableProduct) {
+      setName(recyclableProduct.name || '')
+      setBrand(recyclableProduct.brand || '')
+      setCategory(recyclableProduct.category || '')
+      setWeightGr(recyclableProduct.weightGr || '')
+      setDescription(recyclableProduct.description || '')
+      setRecyclingNote(recyclableProduct.recyclingNote || '')
+      setImagesArray(recyclableProduct.images || [])
+      setBarCode(recyclableProduct.barCode || '')
+    }
+  }, [recyclableProduct])
+
+  // Form submit handler
+  const submitHandler = async (e) => {
+    e.preventDefault()
+
+    const updatedRecyclableProduct = {
+      _id: recyclableProductId,
+      name,
+      brand,
+      category,
+      weightGr,
+      description,
+      recyclingNote,
+      images: imagesArray,
+      barCode,
+    }
+
+    try {
+      await updateRecyclableProduct(updatedRecyclableProduct).unwrap()
+      toast.success('Produit recyclable mis à jour avec succès!')
+      refetch()
+      navigate('/admin-produits-recyclable')
+    } catch (err) {
+      console.error('Erreur lors de la mise à jour du produit recyclable:', err)
+      toast.error('Erreur lors de la mise à jour du produit recyclable')
+    }
+  }
+
+  // Image upload handler
+  const uploadFileHandler = async (e) => {
+    const formData = new FormData()
+    formData.append('images', e.target.files[0])
+
+    try {
+      const res = await uploadRecyclableProductImage(formData).unwrap()
+      setImagesArray((prevImages) => [...prevImages, ...res.images])
+    } catch (err) {
+      toast.error(err?.data?.message || err.error)
+    }
+  }
+
+  // Remove image handler
+  const removeImage = (index) => {
+    const newImagesArray = [...imagesArray]
+    newImagesArray.splice(index, 1)
+    setImagesArray(newImagesArray)
+  }
+
+  if (recyclableProductLoading) {
+    return <Loader />
+  }
+
+  if (recyclableProductError) {
+    return <p className="text-red-500">Erreur: {recyclableProductError.message}</p>
+  }
+
   return (
     <>
-    <div className='flex items-center justify-between'>
-    <h1 className='text-2xl text-gray-800' >Editer le produit recyclable : {recyclableProductId}</h1>
-    <Button url={'/admin-produits-recyclable'}> <ArrowBigLeft/>Retour</Button>
-    </div>
+      <div className="mb-10 flex items-center justify-between">
+        <h1 className="text-2xl text-gray-800">Éditer le produit recyclable : {recyclableProductId}</h1>
+        <Button url={'/admin-produits-recyclable'}><ArrowBigLeft /> Retour</Button>
+      </div>
+
+      <form onSubmit={submitHandler}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Nom</label>
+          <input
+            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Marque</label>
+          <input
+            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={brand}
+            onChange={(e) => setBrand(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Catégorie</label>
+          <select
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="" disabled>Sélectionner une catégorie</option>
+            {categories.map((cat) => (
+              <option key={cat} value={cat}>{cat}</option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Poids (g)</label>
+          <input
+            type="number"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={weightGr}
+            onChange={(e) => setWeightGr(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Description</label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Note de Recyclage</label>
+          <select
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={recyclingNote}
+            onChange={(e) => setRecyclingNote(e.target.value)}
+          >
+            <option value="" disabled>Sélectionner une note</option>
+            {recyclingNotes.map((note) => (
+              <option key={note} value={note}>{note}</option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Images</label>
+          <div className="flex flex-wrap">
+            {imagesArray.length > 0 ? (
+              imagesArray.map((image, index) => (
+                <div key={index} className="relative mr-4 mb-4">
+                  <img
+                    src={image}
+                    alt={`Image ${index + 1}`}
+                    className="w-32 h-32 object-cover"
+                  />
+                  <button
+                    type="button"
+                    className="absolute top-0 right-0 bg-red-500 text-white p-1 rounded"
+                    onClick={() => removeImage(index)}
+                  >
+                    X
+                  </button>
+                </div>
+              ))
+            ) : (
+              <p>Aucune image ajoutée</p>
+            )}
+          </div>
+          <input
+            type="file"
+            multiple
+            onChange={uploadFileHandler}
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">Code-barres</label>
+          <input
+            type="text"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            value={barCode}
+            onChange={(e) => setBarCode(e.target.value)}
+          />
+        </div>
+
+        <Button type="submit" icon={Send}>
+          {loadingUpdate ? <Loader2 /> : 'Valider'}
+        </Button>
+      </form>
     </>
   )
 }
