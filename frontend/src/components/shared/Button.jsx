94f8c3af98cb6a4bbbf8af6633ae9d36efda3ef9
Velten Stoyann <krysto.dev@gmail.com>
2024-07-21 00:36:04 +1100

    update shared btn component for take link in props

--- frontend/src/components/shared/Button.jsx
+++ frontend/src/components/shared/Button.jsx
@@ -1,37 +1,51 @@
-const Button = ({ children, icon: Icon, version, type = 'button', isDisabled = false, onClick }) => {
-    const baseStyle = 'flex items-center gap-2 justify-center text-xs px-5 py-2 font-semibold rounded-lg shadow-md focus:outline-none transition duration-300';
-    
-    let versionStyle;
-    if (version === 'primary') {
-      versionStyle = 'bg-primaryColor hover:bg-secondaryColor focus:ring-2 focus:ring-secondaryColor focus:ring-opacity-50';
-    } else if (version === 'secondary') {
-      versionStyle = 'bg-secondaryColor hover:bg-primaryColor focus:ring-2 focus:ring-secondaryColor focus:ring-opacity-50';
-    } 
-    else if (version === 'danger') {
-      versionStyle = 'bg-dangerColor hover:bg-red-600 focus:ring-2 focus:ring-secondaryColor focus:ring-opacity-50';
-    } 
-    else if (version === 'warning') {
-      versionStyle = 'bg-warningColor hover:bg-orange-400 focus:ring-2 focus:ring-warningColor focus:ring-opacity-50';
-    } 
-    
-    else {
-      versionStyle = 'bg-green-500 text-white hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50';
-    }
-  
-    const disabledStyle = isDisabled ? 'cursor-not-allowed opacity-50 bg-red-300 hover:bg-red-400' : '';
-  
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Button = ({ children, icon: Icon, version, type = 'button', isDisabled = false, onClick, url }) => {
+  const baseStyle = 'flex items-center gap-2 justify-center text-xs px-5 py-2 font-semibold rounded-lg shadow-md focus:outline-none transition duration-300';
+
+  let versionStyle;
+  if (version === 'primary') {
+    versionStyle = 'bg-primaryColor hover:bg-secondaryColor focus:ring-2 focus:ring-secondaryColor focus:ring-opacity-50';
+  } else if (version === 'secondary') {
+    versionStyle = 'bg-secondaryColor hover:bg-primaryColor focus:ring-2 focus:ring-secondaryColor focus:ring-opacity-50';
+  } else if (version === 'danger') {
+    versionStyle = 'bg-dangerColor hover:bg-red-600 focus:ring-2 focus:ring-secondaryColor focus:ring-opacity-50';
+  } else if (version === 'warning') {
+    versionStyle = 'bg-warningColor hover:bg-orange-400 focus:ring-2 focus:ring-warningColor focus:ring-opacity-50';
+  } else {
+    versionStyle = 'bg-green-500 text-white hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50';
+  }
+
+  const disabledStyle = isDisabled ? 'cursor-not-allowed opacity-50 bg-red-300 hover:bg-red-400' : '';
+
+  if (url) {
+    // Render as an anchor element if `href` is provided
     return (
-      <button
-        type={type}
-        disabled={isDisabled}
-        onClick={onClick}
+      <Link
+        to={url}
         className={`${baseStyle} ${versionStyle} ${disabledStyle}`}
+        onClick={isDisabled ? (e) => e.preventDefault() : onClick}
+        aria-disabled={isDisabled ? 'true' : undefined} // Optional: add aria-disabled for accessibility
       >
         {Icon && <Icon className="w-5 h-5" />}
         {children}
-      </button>
+      </Link>
     );
-  };
-  
-  export default Button;
-  
+  }
+
+  // Default to rendering a button element
+  return (
+    <button
+      type={type}
+      disabled={isDisabled}
+      onClick={onClick}
+      className={`${baseStyle} ${versionStyle} ${disabledStyle}`}
+    >
+      {Icon && <Icon className="w-5 h-5" />}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
--- frontend/src/screens/admin/AdminPlasticsScreen.jsx
+++ frontend/src/screens/admin/AdminPlasticsScreen.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import { useCreatePlasticTypeMutation, useDeletePlasticTypeMutation, useGetPlasticTypesQuery } from '../../slices/plasticTypesSlice';
-import Button from '../../components/shared/Button';
-import { Edit, EyeIcon, Loader2, PlusCircle, Trash } from 'lucide-react';
-import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import Loader from '../FeedbackScreens/Loader';
+import React from 'react'
+import {
+  useCreatePlasticTypeMutation,
+  useDeletePlasticTypeMutation,
+  useGetPlasticTypesQuery,
+} from '../../slices/plasticTypesSlice'
+import Button from '../../components/shared/Button'
+import { Edit, EyeIcon, Loader2, PlusCircle, Trash } from 'lucide-react'
+import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import Loader from '../FeedbackScreens/Loader'
 
 const AdminPlasticsScreen = () => {
-  const { data: plasticTypes, error: plasticTypesError, isLoading: plasticTypesLoading , refetch } = useGetPlasticTypesQuery();
-
+  const {
+    data: plasticTypes,
+    error: plasticTypesError,
+    isLoading: plasticTypesLoading,
+    refetch,
+  } = useGetPlasticTypesQuery()
 
   const [
     createPlasticType,
     { isLoading: loadingCreate },
-    
   ] = useCreatePlasticTypeMutation()
 
   const [
@@ -21,8 +28,9 @@ const AdminPlasticsScreen = () => {
     { isLoading: deletePlasticTypeLoading },
   ] = useDeletePlasticTypeMutation()
 
-  if (plasticTypesLoading) return <Loader/>;
-  if (plasticTypesError) return <p className="text-red-500">Erreur: {plasticTypesError.message}</p>;
+  if (plasticTypesLoading) return <Loader />
+  if (plasticTypesError)
+    return <p className="text-red-500">Erreur: {plasticTypesError.message}</p>
   const createPlasticTypeHandler = async () => {
     if (window.confirm('Voulez-vous ajouter un produit?')) {
       try {
@@ -41,62 +49,87 @@ const AdminPlasticsScreen = () => {
         await deletePlasticType(id).unwrap()
         toast.success('Type de plastique supprimé avec succès!')
         refetch()
-        
       } catch (err) {
         toast.error('Erreur lors de la suppression du type de plastique')
       }
     }
   }
 
-  if (deletePlasticTypeLoading) return <Loader/>
+  if (deletePlasticTypeLoading) return <Loader />
 
   return (
     <>
-      <h1 className='text-gray-800 text-2xl mb-6'>Types de Plastique</h1>
+    <div className='flex items-center justify-between mb-8'>
+
+      <h1 className="text-gray-800 text-2xl ">Types de Plastique</h1>
       <Button
-          onClick={createPlasticTypeHandler}
-          icon={PlusCircle}
-          version="success"
+        onClick={createPlasticTypeHandler}
+        icon={PlusCircle}
+        version="success"
         >
-          {loadingCreate ? <Loader2/> : 'Ajouter'}
-        </Button>
+        {loadingCreate ? <Loader2 /> : 'Ajouter'}
+      </Button>
+        </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {plasticTypes.map(plastic => (
-          <div key={plastic._id} className="bg-white shadow-md rounded-lg overflow-hidden">
+        {plasticTypes.map((plastic) => (
+          <div
+            key={plastic._id}
+            className="bg-white shadow-md rounded-lg overflow-hidden"
+          >
             <img
               src={plastic.images[0] || '/uploads/no-photo.png'}
               alt={plastic.scientificNameFr}
               className="w-full h-56 object-cover"
             />
             <div className="p-4">
-              <h2 className="text-lg font-semibold">{plastic.sigleFr} ({plastic.sigleEn})</h2>
-              <p className="text-gray-600 mt-2">{plastic.scientificNameFr} ({plastic.scientificNameEn})</p>
-             
+              <h2 className="text-lg font-semibold">
+                {plastic.sigleFr} ({plastic.sigleEn})
+              </h2>
+              <p className="text-gray-600 mt-2">
+                {plastic.scientificNameFr} ({plastic.scientificNameEn})
+              </p>
+
               <div className="mt-4">
-               
-                <p className="text-sm text-gray-500"><strong>Impact Environnemental:</strong> {plastic.environmentalImpact}</p>
-                <p className="text-sm text-gray-500"><strong>Température d'Injection:</strong> {plastic.injectionTemperature}</p>
-                <p className="text-sm text-gray-500"><strong>Point de Fusion:</strong> {plastic.meltingPoint}°C</p>
-                <p className="text-sm text-gray-500"><strong>Densité:</strong> {plastic.density}</p>
+                <p className="text-sm text-gray-500">
+                  <strong>Impact Environnemental:</strong>{' '}
+                  {plastic.environmentalImpact}
+                </p>
+                <p className="text-sm text-gray-500">
+                  <strong>Température d'Injection:</strong>{' '}
+                  {plastic.injectionTemperature}
+                </p>
+                <p className="text-sm text-gray-500">
+                  <strong>Point de Fusion:</strong> {plastic.meltingPoint}°C
+                </p>
+                <p className="text-sm text-gray-500">
+                  <strong>Densité:</strong> {plastic.density}
+                </p>
               </div>
-              <div className='mb-4 flex justify-center items-center gap-5' >
-                <Button icon={EyeIcon}>
-                  <Link to={`/admin/plastic-type-details/${plastic._id}`}>Voir</Link>
-                </Button>
-                <Button version={"warning"} icon={Edit}>
-                  <Link to={`/admin/plastic-type-modifier/${plastic._id}`}>Editer</Link>
-                </Button>
-                <Button version={"danger"} icon={Trash}>
-                  <Link to={`/admin/plastic-type-modifier/${plastic._id}`}>Supprimer</Link>
-                </Button>
-               
+              <div className="mb-4 flex justify-center items-center gap-5">
+                <Button
+                version={"primary"}
+                  url={`/admin/plastic-type-details/${plastic._id}`}
+                  icon={EyeIcon}
+                />
+
+                <Button
+                  url={`/admin/plastic-type-modifier/${plastic._id}`}
+                  version={'warning'}
+                  icon={Edit}
+                />
+
+                <Button
+                  onClick={() => deleteHandler(plastic._id)}
+                  version={'danger'}
+                  icon={Trash}
+                />
               </div>
             </div>
           </div>
         ))}
       </div>
     </>
-  );
+  )
 }
 
-export default AdminPlasticsScreen;
+export default AdminPlasticsScreen
